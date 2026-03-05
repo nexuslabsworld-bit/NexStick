69af9a83d391547edc72e1b2d8fc3240a120d17f
nexuslabsworld-bit <nexuslabsworld@gmail.com>
2026-03-05 08:39:16 +0100

    Add Vercel Analytics plugin to Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { Analytics } from "@vercel/analytics/react"
 export default defineConfig({
   plugins: [react()],
 })
